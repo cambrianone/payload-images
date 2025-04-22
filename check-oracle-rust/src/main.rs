d7020ebd8aba91df5e7d3b8ca674e3b728bed492
itssoeasy <easyawesome@yandex.ru>
2025-04-23 01:18:50 +1000

    feat: add cambrian rust sdk

--- check-oracle-rust/src/main.rs
+++ check-oracle-rust/src/main.rs
@@ -2,47 +2,9 @@ use std::env;
 use std::str::FromStr;
 
 use anyhow::{Context, Result};
-use serde::{Deserialize, Serialize};
+use cambrian_rust_sdk::{Input, Response};
+use solana_program::instruction::{AccountMeta, Instruction};
 use solana_program::pubkey::Pubkey;
-use serde_repr::Serialize_repr;
-
-#[derive(Serialize_repr)]
-#[repr(u8)]
-enum AccountRole {
-    // Bitflag guide: is signer ⌄⌄ is writable
-    WritableSigner = /* 3 */ 0b11,
-    ReadonlySigner = /* 2 */ 0b10,
-    Writable =        /* 1 */ 0b01,
-    Readonly =        /* 0 */ 0b00,
-}
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Input {
-    poa_name: String,
-    proposal_storage_key: String,
-}
-
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-struct AccountMeta {
-    address: String,
-    role: AccountRole,
-}
-
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-struct ProposalInstruction {
-    program_address: String,
-    accounts: Vec<AccountMeta>,
-    data: Vec<u8>,
-}
-
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-struct Response {
-    proposal_instructions: Vec<ProposalInstruction>,
-}
 
 fn main() -> Result<()> {
     let input: Input = serde_json::from_str(&env::var("CAMB_INPUT").unwrap_or_default())
@@ -50,7 +12,7 @@ fn main() -> Result<()> {
 
     let solana_program = Pubkey::from_str("FGgNUqGxdEYM1gVtQT5QcTbzNv4y1UPoVvXPRnooBdxo")?;
     let oracle_program = Pubkey::from_str("ECb6jyKXDTE8NjVjsKgNpjSjcv4h2E7JQ42yKqWihBQE")?;
-    let sysvar_instructions = "Sysvar1nstructions1111111111111111111111111";
+    let sysvar_instructions = Pubkey::from_str("Sysvar1nstructions1111111111111111111111111")?;
 
     let poa_state_key = input.poa_name.as_bytes();
     let storage_space: u32 = 3 * 25;
@@ -67,33 +29,33 @@ fn main() -> Result<()> {
     let (poa_state_pda, _) =
         Pubkey::find_program_address(&[b"STATE", poa_state_key], &solana_program);
 
-    let instruction = ProposalInstruction {
-        program_address: oracle_program.to_string(),
+    let instruction = Instruction {
+        program_id: oracle_program,
         accounts: vec![
             AccountMeta {
-                address: proposal_storage_pda.to_string(),
-                role: AccountRole::Writable,
+                pubkey: proposal_storage_pda,
+                is_signer: false,
+                is_writable: true,
             },
             AccountMeta {
-                address: poa_state_pda.to_string(),
-                role: AccountRole::Readonly
+                pubkey: poa_state_pda,
+                is_signer: false,
+                is_writable: false,
             },
             AccountMeta {
-                address: sysvar_instructions.to_string(),
-                role: AccountRole::Readonly,
+                pubkey: sysvar_instructions,
+                is_signer: false,
+                is_writable: false,
             },
             AccountMeta {
-                address: oracle_program.to_string(),
-                role: AccountRole::Readonly,
+                pubkey: oracle_program,
+                is_signer: false,
+                is_writable: false,
             },
         ],
         data: poa_state_key.to_vec(),
     };
 
-    let response = Response {
-        proposal_instructions: vec![instruction],
-    };
-
-    println!("{}", serde_json::to_string_pretty(&response)?);
+    println!("{}", Response::from(instruction).to_output_ix()?);
     Ok(())
 }
